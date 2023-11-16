6c484bef4861f403d25a4bd0ee5cfac41886d83c
David <davidmunyiri2@gmail.com>
2023-11-16 11:58:39 +0300

    created Post and Category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    """
+    Model for defining blog category attributes
+    """
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    """
+    Model for defining blog post attribites
+    """
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+    
