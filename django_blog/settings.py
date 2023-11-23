ae0c90aced4a156f930b2df48bba37eb9c75a7df
David <davidmunyiri2@gmail.com>
2023-11-23 11:11:58 +0300

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -148,6 +148,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog:homepage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # HTTPS setting
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
